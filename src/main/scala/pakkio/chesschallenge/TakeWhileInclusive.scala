5e205a9412edd9befc39ddde9073e46008ab589d
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2015-06-22 01:24:04 +0200

    documenting TakeWhileInclusive.scala.. If you are interested in its usage check older commits when I used it for the shadowing checks.

--- src/main/scala/pakkio/chesschallenge/TakeWhileInclusive.scala
+++ src/main/scala/pakkio/chesschallenge/TakeWhileInclusive.scala
@@ -1,5 +1,8 @@
 package pakkio.chesschallenge
 
+// this is an interesting addition to Iterator allowing to do takeWhile including the first non matching element
+// it can be very useful for instance in dealing with shadowing when we follow a ray from a piece
+// understanding when the first slot is not free...
 class IteratorExtension[A](i : Iterator[A]) {
   def takeWhileInclusive(p: A => Boolean) = {
     val (a, b) = i.span(p)
